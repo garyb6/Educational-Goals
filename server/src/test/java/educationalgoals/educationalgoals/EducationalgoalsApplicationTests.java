1b4da3e503b22c67f2ae9bc0b6be737227efe20a
garyb6 <gary.bennett6@outlook.com>
2022-02-28 14:45:04 +0000

    can save quiz test passed

--- server/src/test/java/educationalgoals/educationalgoals/EducationalgoalsApplicationTests.java
+++ server/src/test/java/educationalgoals/educationalgoals/EducationalgoalsApplicationTests.java
@@ -1,13 +1,41 @@
 package educationalgoals.educationalgoals;
 
+import educationalgoals.educationalgoals.models.Option;
+import educationalgoals.educationalgoals.models.Quiz;
+import educationalgoals.educationalgoals.repositories.OptionRespository;
+import educationalgoals.educationalgoals.repositories.QuestionRepository;
+import educationalgoals.educationalgoals.repositories.QuizRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class EducationalgoalsApplicationTests {
 
+	@Autowired
+	QuizRepository quizRepository;
+
+	@Autowired
+	OptionRespository optionRespository;
+
+	@Autowired
+	QuestionRepository questionRepository;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void canSaveQuiz(){
+		Quiz roman = new Quiz("Roman");
+		quizRepository.save(roman);
+
+//	@Test
+//	public void canSaveFalseOption(){
+//		Option po = new Option("The Po", romanRiver);
+//		optionRespository.save(po);
+
+
 	}
 
 }
