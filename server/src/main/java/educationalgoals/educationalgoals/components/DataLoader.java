f1781b3c05d77c9f45c77bb086e28d36444533de
garyb6 <gary.bennett6@outlook.com>
2022-02-28 11:03:13 +0000

    dataloader set up ready for api creation

--- server/src/main/java/educationalgoals/educationalgoals/components/DataLoader.java
+++ server/src/main/java/educationalgoals/educationalgoals/components/DataLoader.java
@@ -1,4 +1,25 @@
 package educationalgoals.educationalgoals.components;
 
-public class DataLoader {
+import educationalgoals.educationalgoals.repositories.AnswerRepository;
+import educationalgoals.educationalgoals.repositories.QuestionRepository;
+import educationalgoals.educationalgoals.repositories.QuizRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements ApplicationRunner {
+
+    @Autowired
+    QuizRepository quizRepository;
+    AnswerRepository answerRepository;
+    QuestionRepository questionRepository;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+
+
+    }
 }
