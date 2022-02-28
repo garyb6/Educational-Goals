ff6ae660f7087c6186e0a2c18b47f422849c6979
Fraser Dillon <fraserdillon050388@gmail.com>
2022-02-28 15:08:04 +0000

    Science quiz and Q1 added

--- server/src/main/java/educationalgoals/educationalgoals/components/DataLoader.java
+++ server/src/main/java/educationalgoals/educationalgoals/components/DataLoader.java
@@ -29,18 +29,52 @@ public class DataLoader implements ApplicationRunner {
     public void run(ApplicationArguments args) {
         Quiz roman = new Quiz("Roman");
         quizRepository.save(roman);
+
         Question romanRiver = new Question (roman, "Which river was the city of Rome built next to?");
+        questionRepository.save(romanRiver);
+
         Option tiber = new Option("The Tiber", romanRiver, true);
         optionRespository.save(tiber);
         romanRiver.addOption(tiber);
+
         Option po = new Option("The Po", romanRiver);
         optionRespository.save(po);
+        romanRiver.addOption(po);
+
         Option arno = new Option("The Arno", romanRiver);
         optionRespository.save(arno);
         romanRiver.addOption(arno);
+
         Option rhine = new Option("The Rhine", romanRiver);
         optionRespository.save(rhine);
         romanRiver.addOption(rhine);
+
         roman.addQuestion(romanRiver);
+
+        Quiz science = new Quiz("Science");
+        quizRepository.save(science);
+
+        Question scienceBones = new Question (science, "How many bones are there in the human body?");
+        questionRepository.save(scienceBones);
+
+        Option twoZeroSix = new Option("206", scienceBones, true);
+        optionRespository.save(twoZeroSix);
+        scienceBones.addOption(twoZeroSix);
+
+        Option onceTwoZeroNine = new Option("1209", scienceBones);
+        optionRespository.save(onceTwoZeroNine);
+        scienceBones.addOption(onceTwoZeroNine);
+
+
+        Option threeSevenThree = new Option("373", scienceBones);
+        optionRespository.save(threeSevenThree);
+        romanRiver.addOption(threeSevenThree);
+
+        Option fiveFiveThree = new Option("553", scienceBones);
+        optionRespository.save(fiveFiveThree);
+        scienceBones.addOption(fiveFiveThree);
+
+        science.addQuestion(scienceBones);
+
     }
 }
