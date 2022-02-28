d28796a5ae7c135a4558badedfb630d9623948a0
garyb6 <gary.bennett6@outlook.com>
2022-02-28 15:16:53 +0000

    added firstEmperor Question

--- server/src/main/java/educationalgoals/educationalgoals/components/DataLoader.java
+++ server/src/main/java/educationalgoals/educationalgoals/components/DataLoader.java
@@ -32,23 +32,38 @@ public class DataLoader implements ApplicationRunner {
 
         Question romanRiver = new Question (roman, "Which river was the city of Rome built next to?");
         questionRepository.save(romanRiver);
-
         Option tiber = new Option("The Tiber", romanRiver, true);
         optionRespository.save(tiber);
         romanRiver.addOption(tiber);
-
         Option po = new Option("The Po", romanRiver);
         optionRespository.save(po);
         romanRiver.addOption(po);
-
         Option arno = new Option("The Arno", romanRiver);
         optionRespository.save(arno);
         romanRiver.addOption(arno);
-
         Option rhine = new Option("The Rhine", romanRiver);
         optionRespository.save(rhine);
         romanRiver.addOption(rhine);
 
         roman.addQuestion(romanRiver);
+
+        Question firstEmperor = new Question (roman, "What was the name of the first emperor of Rome in 27 BC?");
+        questionRepository.save(firstEmperor);
+        Option tiberius = new Option("Tiberius", firstEmperor);
+        optionRespository.save(tiberius);
+        firstEmperor.addOption(tiberius);
+        Option claudius = new Option("Claudius", firstEmperor);
+        optionRespository.save(claudius);
+        firstEmperor.addOption(claudius);
+        Option vespasian = new Option("Vespasian", firstEmperor);
+        optionRespository.save(vespasian);
+        firstEmperor.addOption(vespasian);
+        Option augustus = new Option("Augustus", firstEmperor, true);
+        optionRespository.save(augustus);
+        firstEmperor.addOption(augustus);
+
+        roman.addQuestion(firstEmperor);
+
+        
     }
 }
