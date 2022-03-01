3e8e545b9ff524a4eb2caceca9c4aee9b6d07df1
garyb6 <gary.bennett6@outlook.com>
2022-03-01 13:57:09 +0000

    initial carousel created

--- client/src/containers/StartContainer.js
+++ client/src/containers/StartContainer.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
+import { Carousel } from 'react-bootstrap';
 
 
 
@@ -22,18 +23,49 @@ const StartContainer = ({playerOne, playerTwo, setPlayerOne, setPlayerTwo}) => {
         <>
         <h1>Educational Goals</h1>
         <input type = "text" onChange={handleChangePlayerOne} id = "Player 1 input" /> 
-        <h2> This is the carousel </h2> 
-        <input type = "text" onChange={handleChangePlayerTwo} id = "Player 2 input"/> 
-        <h2> This is the carousel </h2> 
-        <form>
-            <label> Choose Quiz </label>
-            <select>
-            <option value = ""> Jamie's Quiz </option>
-            <option value = ""> Fraser's Quiz </option>
-            <option value = ""> Andrew's Quiz </option>
-            <option value = ""> Gary's Quiz </option>
-            </select>
-        </form>
+        <Carousel variant="dark">
+    <Carousel.Item>
+        <img
+        className="d-block w-100"
+        src="holder.js/800x400?text=First slide&bg=f5f5f5"
+        alt="First slide"
+        />
+        <Carousel.Caption>
+        <h5>First slide label</h5>
+        </Carousel.Caption>
+    </Carousel.Item>
+    <Carousel.Item>
+        <img
+        className="d-block w-100"
+        src="holder.js/800x400?text=Second slide&bg=eee"
+        alt="Second slide"
+        />
+        <Carousel.Caption>
+        <h5>Second slide label</h5>
+        </Carousel.Caption>
+    </Carousel.Item>
+    <Carousel.Item>
+        <img
+        className="d-block w-100"
+        src="holder.js/800x400?text=Third slide&bg=e5e5e5"
+        alt="Third slide"
+        />
+        <Carousel.Caption>
+        <h5>Third slide label</h5>
+        </Carousel.Caption>
+    </Carousel.Item>
+</Carousel>
+    <input type = "text" onChange={handleChangePlayerTwo} id = "Player 2 input"/> 
+    <h2> This is the carousel </h2> 
+    <form>
+        <label> Choose Quiz </label>
+        <select>
+        <option value = ""> Jamie's Quiz </option>
+        <option value = ""> Fraser's Quiz </option>
+        <option value = ""> Andrew's Quiz </option>
+        <option value = ""> Gary's Quiz </option>
+        </select>
+    </form>
         <Link to = "/quiz">Start</Link>
         </>
     
