3c014f467b403ac4795756b4aedbd8eca331f342
garyb6 <gary.bennett6@outlook.com>
2022-03-01 14:45:44 +0000

    css added to page display

--- client/src/containers/StartContainer.js
+++ client/src/containers/StartContainer.js
@@ -22,56 +22,99 @@ const StartContainer = ({playerOne, playerTwo, setPlayerOne, setPlayerTwo}) => {
     }
 
     return (
-        <>
-        <h1>Educational Goals</h1>
-        <input type = "text" onChange={handleChangePlayerOne} id = "Player 1 input" /> 
-        <Carousel variant="dark">
-    <Carousel.Item>
-        <img
-        className="d-block w-100"
-        src= {Crouch}
-        alt="Goalkeeper"
-        />
-        <Carousel.Caption>
-        <h5>Goalkeeper</h5>
-        </Carousel.Caption>
-    </Carousel.Item>
-    <Carousel.Item>
-        <img
-        className="d-block w-100"
-        src={Stand}
-        alt="Striker"
-        style={{filter: "invert(18%) sepia(75%) saturate(5707%) hue-rotate(223deg) brightness(102%) contrast(105%)"}}
-        />
-        <Carousel.Caption>
-        <h5>Striker</h5>
-        </Carousel.Caption>
-    </Carousel.Item>
-    <Carousel.Item>
-        <img
-        className="d-block w-100"
-        src={JumpL}
-        alt="Diving Goalkeeper"
-        />
-        <Carousel.Caption>
-        <h5>Diving Goalkeeper</h5>
-        </Carousel.Caption>
-    </Carousel.Item>
-</Carousel>
-    <input type = "text" onChange={handleChangePlayerTwo} id = "Player 2 input"/> 
-    <h2> This is the carousel </h2> 
-    <form>
-        <label> Choose Quiz </label>
-        <select>
-        <option value = ""> Jamie's Quiz </option>
-        <option value = ""> Fraser's Quiz </option>
-        <option value = ""> Andrew's Quiz </option>
-        <option value = ""> Gary's Quiz </option>
-        </select>
-    </form>
-        <Link to = "/quiz">Start</Link>
-        </>
-    
+    <>
+        <div className="startContainer">
+            <h1>Educational Goals</h1>
+                <div className="playerNameBoxes">
+                    <label for="player1Box">Player 1:</label>
+                    <input id = "player1Box" type = "text" onChange={handleChangePlayerOne} id = "Player 1 input" />
+                    <label for="player2Box">Player 2:</label>
+                    <input id = "player2Box" type = "text" onChange={handleChangePlayerTwo} id = "Player 2 input"/> 
+                </div>
+                <div className="colourSelector">
+                    <Carousel variant="dark">
+                        <Carousel.Item>
+                            <img
+                            className="d-block w-100"
+                            src= {Crouch}
+                            alt="Goalkeeper"
+                            />
+                            <Carousel.Caption>
+                            <h5>Goalkeeper</h5>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                            className="d-block w-100"
+                            src={Stand}
+                            alt="Striker"
+                            style={{filter: "invert(18%) sepia(75%) saturate(5707%) hue-rotate(223deg) brightness(102%) contrast(105%)"}}
+                            />
+                            <Carousel.Caption>
+                            <h5>Striker</h5>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                            className="d-block w-100"
+                            src={JumpL}
+                            alt="Diving Goalkeeper"
+                            />
+                            <Carousel.Caption>
+                            <h5>Diving Goalkeeper</h5>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                    </Carousel>
+
+                    <Carousel variant="dark">
+                        <Carousel.Item>
+                            <img
+                            className="d-block w-100"
+                            src= {Crouch}
+                            alt="Goalkeeper"
+                            />
+                            <Carousel.Caption>
+                            <h5>Goalkeeper</h5>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                            className="d-block w-100"
+                            src={Stand}
+                            alt="Striker"
+                            style={{filter: "invert(18%) sepia(75%) saturate(5707%) hue-rotate(223deg) brightness(102%) contrast(105%)"}}
+                            />
+                            <Carousel.Caption>
+                            <h5>Striker</h5>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                            className="d-block w-100"
+                            src={JumpL}
+                            alt="Diving Goalkeeper"
+                            />
+                        <Carousel.Caption>
+                            <h5>Diving Goalkeeper</h5>
+                        </Carousel.Caption>
+                        </Carousel.Item>
+                    </Carousel>
+            </div>
+            <div className="quizSelector">
+                    <form>
+                        <label> Choose Quiz </label>
+                        <select>
+                        <option value = ""> Jamie's Quiz </option>
+                        <option value = ""> Fraser's Quiz </option>
+                        <option value = ""> Andrew's Quiz </option>
+                        <option value = ""> Gary's Quiz </option>
+                        </select>
+                    </form>
+                    <Link to = "/quiz">Start</Link>
+            </div>
+        </div>
+    </>
+
     )
     
 }
