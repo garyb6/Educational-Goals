ccc0c716175bca875d61fce33eac7c4d09994489
Jamie <jamienolan93@hotmail.co.uk>
2022-03-03 07:59:24 +0000

    added game component

--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -1,96 +1,83 @@
+import React from 'react';
+
+
 const Game = () => {
 
-    const playerOneInput = []
-    const playerTwoInput = []
-  
-    const playerDirection = (event) => {
-  
-      if (event.key === "d"){
-  
-      playerOneInput.push("d")
-  
-      console.log('shoot right', event.keyCode);
-      }
-  
-      else if (event.key === "s"){
-  
-        playerOneInput.push("s")
-        console.log('shoot middle', event.keyCode)
-      }
-  
-      else if (event.key === "a"){
-        playerOneInput.push("a")
-        console.log('shoot left', event.keyCode)
-      }
-  
-      else if (event.key === "l"){
-        playerTwoInput.push("l")
-        console.log('save right', event.keyCode);
+  const playerOneInput = []
+  const playerTwoInput = []
+
+  const playerDirection = (event) => {
+
+    if (event.key === "d"){
+
+    playerOneInput.push("d")
+
+    console.log('p1 right', event.keyCode);
     }
-  
-    else if (event.key === "k"){
-      playerTwoInput.push("k")
-      console.log('save middle', event.keyCode)
+
+    else if (event.key === "s"){
+
+      playerOneInput.push("s")
+      console.log('p1 middle', event.keyCode)
     }
-  
-    else if (event.key === "j"){
-      playerTwoInput.push("j")
-      console.log('save left', event.keyCode)
+
+    else if (event.key === "a"){
+      playerOneInput.push("a")
+      console.log('p1 left', event.keyCode)
     }
-    };
-  
-  
-    React.useEffect(() => {
-  
-      window.addEventListener('keydown', playerDirection);
-  
-  
-  
-      return () => {
-  
-        window.removeEventListener('keydown', playerDirection);
-  
-      };
-  
-    }, []);
-  
-    const checkGoal = () => {
-      const playerOneFinalInput = playerOneInput.slice(-1)[0]
-      const playerTwoFinalInput = playerTwoInput.slice(-1)[0]
-  
-      if (playerOneFinalInput == ["d"] && playerTwoFinalInput == ["l"]){
-        return console.log("SAVE")
-      } 
-      
-      else if (playerOneFinalInput == ["s"] && playerTwoFinalInput == ["k"]){
-        return console.log("SAVE")
-      }
-  
-      else if (playerOneFinalInput == ["a"] && playerTwoFinalInput == ["j"]){
-        return console.log("SAVE")
-      }
-  
-      else {
-        return console.log("GOOOOOOOOOL")
-      }
-  
-    }
-  
-  
-    return (
-  
-      
-  
-      <div className='container'>
-  
-        <h1>hi</h1>
-  
-      </div>
-  
-    );
-  
+
+    else if (event.key === "l"){
+      playerTwoInput.push("l")
+      console.log('p2 right', event.keyCode);
+  }
+
+  else if (event.key === "k"){
+    playerTwoInput.push("k")
+    console.log('p2 middle', event.keyCode)
+  }
+
+  else if (event.key === "j"){
+    playerTwoInput.push("j")
+    console.log('p2 left', event.keyCode)
+  }
   };
-  
-  
-  export default Game;
-  
+
+
+  React.useEffect(() => {
+
+    window.addEventListener('keydown', playerDirection);
+
+
+
+    return () => {
+
+      window.removeEventListener('keydown', playerDirection);
+
+    };
+
+  }, []);
+
+  const checkGoal = () => {
+    const playerOneFinalInput = playerOneInput.slice(-1)[0]
+    const playerTwoFinalInput = playerTwoInput.slice(-1)[0]
+    const combinedInput = playerOneFinalInput.concat(playerTwoFinalInput)
+
+    
+    if (combinedInput == ["d", "l"]){
+      return console.log("SAVE")
+    } 
+    
+    else if (combinedInput == ["s","k"]){
+      return console.log("SAVE")
+    }
+
+    else if (combinedInput == ["a", "j"]){
+      return console.log("SAVE")
+    }
+
+    else {
+      return console.log("GOOOOOOOOOL")
+    }
+
+  }
+};
