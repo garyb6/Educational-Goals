e27165ed251879d818c6e231a47dd02cf5ed5b58
Fraser Dillon <fraserdillon050388@gmail.com>
2022-03-03 11:41:15 +0000

    Score updates

--- client/src/containers/MainContainer.js
+++ client/src/containers/MainContainer.js
@@ -1,5 +1,5 @@
-import React, {useState, useEffect} from 'react';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import StartContainer from './StartContainer';
 import GameContainer from './GameContainer';
 import QuizContainer from './QuizContainer';
@@ -8,40 +8,41 @@ import Request from '../helpers/request';
 
 const MainContainer = () => {
 
-    const [playerOne, setPlayerOne] = useState("Player 1");
-    const [playerTwo, setPlayerTwo] = useState("Player 2");
+    const [playerOne, setPlayerOne] = useState({ name: "Player 1", score: 0 });
+    const [playerTwo, setPlayerTwo] = useState({ name: "Player 2", score: 0 });
     const [selectedQuiz, setSelectedQuiz] = useState(1);
     const [chosenQuiz, setChosenQuiz] = useState(null);
     const [questionNumber, setQuestionNumber] = useState(1)
 
+
     const [quizzes, setQuizzes] = useState([]);
-    useEffect(()=>{
+    useEffect(() => {
         requestAll()
     }, [])
 
-    const requestAll = function(){
+    const requestAll = function () {
         const request = new Request();
         request.get('http://localhost:8080/quizzes')
-        .then((data) => setQuizzes(data))
+            .then((data) => setQuizzes(data))
     }
 
-    const requestQuiz = function(id){
-      const request = new Request();
-      request.get(`http://localhost:8080/quizzes/${id}`)
-      .then((data) => setChosenQuiz(data))
-  }
+    const requestQuiz = function (id) {
+        const request = new Request();
+        request.get(`http://localhost:8080/quizzes/${id}`)
+            .then((data) => setChosenQuiz(data))
+    }
 
-  return (
+    return (
 
-    <Router>
-        <Routes>
-            <Route path = "/start" element={<StartContainer playerOne = {playerOne} playerTwo = {playerTwo} setPlayerOne = {setPlayerOne} setPlayerTwo = {setPlayerTwo} quizzes={quizzes} setSelectedQuiz = {setSelectedQuiz}/>}/>
-            <Route path = "/quiz" element={<QuizContainer playerOne = {playerOne} playerTwo = {playerTwo} requestQuiz = {requestQuiz} selectedQuiz = {selectedQuiz} chosenQuiz = {chosenQuiz} questionNumber = {questionNumber} setQuestionNumber = {setQuestionNumber}/>}/>
-            <Route path = "/game" element={<GameContainer playerOne = {playerOne} playerTwo = {playerTwo}/>}/>
-        </Routes>
-    </Router>
+        <Router>
+            <Routes>
+                <Route path="/start" element={<StartContainer playerOne={playerOne} playerTwo={playerTwo} setPlayerOne={setPlayerOne} setPlayerTwo={setPlayerTwo} quizzes={quizzes} setSelectedQuiz={setSelectedQuiz} />} />
+                <Route path="/quiz" element={<QuizContainer playerOne={playerOne} playerTwo={playerTwo} requestQuiz={requestQuiz} selectedQuiz={selectedQuiz} chosenQuiz={chosenQuiz} questionNumber={questionNumber} setQuestionNumber={setQuestionNumber} />} />
+                <Route path="/game" element={<GameContainer playerOne={playerOne} playerTwo={playerTwo} setPlayerOne={setPlayerOne} setPlayerTwo={setPlayerTwo} questionNumber={questionNumber} />} />
+            </Routes>
+        </Router>
 
-  );
+    );
 }
 
 export default MainContainer;
--- client/src/containers/StartContainer.js
+++ client/src/containers/StartContainer.js
@@ -1,21 +1,26 @@
-import React, {useState} from 'react';
-import {Link} from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import { Carousel } from 'react-bootstrap';
 import Crouch from "./../images/Crouch.png";
 import Stand from "./../images/Stand.png";
 import JumpL from "./../images/Jump_left.png";
 import "./../css/StartContainer.css"
 
-const StartContainer = ({playerOne, playerTwo, setPlayerOne, setPlayerTwo, quizzes, setSelectedQuiz}) => {
-    
+const StartContainer = ({ playerOne, playerTwo, setPlayerOne, setPlayerTwo, quizzes, setSelectedQuiz }) => {
+
     const handleChangePlayerOne = (event) => {
 
-        setPlayerOne(event.target.value);
+        const tempPlayer = playerOne;
+        tempPlayer.name = event.target.value
+        setPlayerOne(tempPlayer)
+
 
     }
     const handleChangePlayerTwo = (event) => {
 
-        setPlayerTwo(event.target.value);
+        const tempPlayer = playerTwo;
+        tempPlayer.name = event.target.value
+        setPlayerTwo(tempPlayer)
 
     }
 
@@ -25,112 +30,112 @@ const StartContainer = ({playerOne, playerTwo, setPlayerOne, setPlayerTwo, quizz
     }
 
     const listOfQuizzes = quizzes.map((quiz, index) => {
-        return <option key = {index} value = {quiz.id}> {quiz.title} </option>
+        return <option key={index} value={quiz.id}> {quiz.title} </option>
     });
 
 
 
     return (
-    <>  
-        <div className="startContainer">
-            <h1>Educational Goals</h1>
-            <div className="players">
-                <div className="player1Select">
-                    <label for="player1Box">Player 1:</label>
-                    <input id = "player1Box" type = "text" onChange={handleChangePlayerOne}/>
-                    <div className="colourSelector">
-                        <Carousel variant="dark">
-                            <Carousel.Item>
-                                <img
-                                className="d-block"
-                                src= {Stand}
-                                alt="Goalkeeper"
-                                // style={{filter: "invert(18%) sepia(75%) saturate(5707%) hue-rotate(223deg) brightness(102%) contrast(105%)"}}
-                                />
-                                {/* <Carousel.Caption>
+        <>
+            <div className="startContainer">
+                <h1>Educational Goals</h1>
+                <div className="players">
+                    <div className="player1Select">
+                        <label for="player1Box">Player 1:</label>
+                        <input id="player1Box" type="text" onChange={handleChangePlayerOne} />
+                        <div className="colourSelector">
+                            <Carousel variant="dark">
+                                <Carousel.Item>
+                                    <img
+                                        className="d-block"
+                                        src={Stand}
+                                        alt="Goalkeeper"
+                                    // style={{filter: "invert(18%) sepia(75%) saturate(5707%) hue-rotate(223deg) brightness(102%) contrast(105%)"}}
+                                    />
+                                    {/* <Carousel.Caption>
                                 <h5>Goalkeeper</h5>
                                 </Carousel.Caption> */}
-                            </Carousel.Item>
-                            <Carousel.Item>
-                                <img
-                                className="d-block"
-                                src={Stand}
-                                alt="Striker"
-                                // style={{filter: "invert(16%) sepia(96%) saturate(6992%) hue-rotate(357deg) brightness(93%) contrast(118%)"}}
-                                />
-                                {/* <Carousel.Caption>
+                                </Carousel.Item>
+                                <Carousel.Item>
+                                    <img
+                                        className="d-block"
+                                        src={Stand}
+                                        alt="Striker"
+                                    // style={{filter: "invert(16%) sepia(96%) saturate(6992%) hue-rotate(357deg) brightness(93%) contrast(118%)"}}
+                                    />
+                                    {/* <Carousel.Caption>
                                 <h5>Striker</h5>
                                 </Carousel.Caption> */}
-                            </Carousel.Item>
-                            <Carousel.Item>
-                                <img
-                                className="d-block"
-                                src={Stand}
-                                alt="Diving Goalkeeper"
-                                // style={{filter: "invert(53%) sepia(28%) saturate(5622%) hue-rotate(88deg) brightness(123%) contrast(122%)"}}
-                                />
-                                {/* <Carousel.Caption>
+                                </Carousel.Item>
+                                <Carousel.Item>
+                                    <img
+                                        className="d-block"
+                                        src={Stand}
+                                        alt="Diving Goalkeeper"
+                                    // style={{filter: "invert(53%) sepia(28%) saturate(5622%) hue-rotate(88deg) brightness(123%) contrast(122%)"}}
+                                    />
+                                    {/* <Carousel.Caption>
                                 <h5>Diving Goalkeeper</h5>
                                 </Carousel.Caption> */}
-                            </Carousel.Item>
-                        </Carousel>
-                    </div>
+                                </Carousel.Item>
+                            </Carousel>
+                        </div>
                     </div>
                     <div className="player2Select">
                         <label for="player2Box">Player 2:</label>
-                        <input id = "player2Box" type = "text" onChange={handleChangePlayerTwo} id = "Player 2 input"/> 
+                        <input id="player2Box" type="text" onChange={handleChangePlayerTwo} id="Player 2 input" />
                         <div className="colourSelector">
-                        <Carousel variant="dark">
-                            <Carousel.Item>
-                                <img
-                                className="d-block"
-                                src= {Stand}
-                                alt="Goalkeeper"
-                                style={{filter: "invert(16%) sepia(96%) saturate(6992%) hue-rotate(357deg) brightness(93%) contrast(118%);"}}
-                                />
-                                {/* <Carousel.Caption>
+                            <Carousel variant="dark">
+                                <Carousel.Item>
+                                    <img
+                                        className="d-block"
+                                        src={Stand}
+                                        alt="Goalkeeper"
+                                        style={{ filter: "invert(16%) sepia(96%) saturate(6992%) hue-rotate(357deg) brightness(93%) contrast(118%);" }}
+                                    />
+                                    {/* <Carousel.Caption>
                                 <p>Goalkeeper</p>
                                 </Carousel.Caption> */}
-                            </Carousel.Item>
-                            <Carousel.Item>
-                                <img
-                                className="d-block"
-                                src={Stand}
-                                alt="Striker"
-                                style={{filter: "invert(53%) sepia(28%) saturate(5622%) hue-rotate(88deg) brightness(123%) contrast(122%)"}}
-                                />
-                                {/* <Carousel.Caption>
+                                </Carousel.Item>
+                                <Carousel.Item>
+                                    <img
+                                        className="d-block"
+                                        src={Stand}
+                                        alt="Striker"
+                                        style={{ filter: "invert(53%) sepia(28%) saturate(5622%) hue-rotate(88deg) brightness(123%) contrast(122%)" }}
+                                    />
+                                    {/* <Carousel.Caption>
                                 <h5>Striker</h5>
                                 </Carousel.Caption> */}
-                            </Carousel.Item>
-                            <Carousel.Item>
-                                <img
-                                className="d-block"
-                                src={Stand}
-                                alt="Diving Goalkeeper"
-                                style={{filter: "invert(18%) sepia(75%) saturate(5707%) hue-rotate(223deg) brightness(102%) contrast(105%)"}}
-                                />
-                            {/* <Carousel.Caption>
+                                </Carousel.Item>
+                                <Carousel.Item>
+                                    <img
+                                        className="d-block"
+                                        src={Stand}
+                                        alt="Diving Goalkeeper"
+                                        style={{ filter: "invert(18%) sepia(75%) saturate(5707%) hue-rotate(223deg) brightness(102%) contrast(105%)" }}
+                                    />
+                                    {/* <Carousel.Caption>
                                 <h5>Diving Goalkeeper</h5>
                             </Carousel.Caption> */}
-                            </Carousel.Item>
-                        </Carousel>
+                                </Carousel.Item>
+                            </Carousel>
                         </div>
                     </div>
                 </div>
-            <div className="quizSelector">
-                <form>
-                    <label> Choose Quiz: </label>
-                    <select onChange = {handleChangeQuizChoice} defaultValue = {1}>
+                <div className="quizSelector">
+                    <form>
+                        <label> Choose Quiz: </label>
+                        <select onChange={handleChangeQuizChoice} defaultValue={1}>
 
-                        {listOfQuizzes}
-                    </select>
-                </form>
-                <Link to = "/quiz">Start</Link>
+                            {listOfQuizzes}
+                        </select>
+                    </form>
+                    <Link to="/quiz">Start</Link>
+                </div>
             </div>
-        </div>
-    </>
-    )    
+        </>
+    )
 }
 
 export default StartContainer;
