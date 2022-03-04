ff7efafd8647cd8bd6b4d2c82dc66f5c709a98e1
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2022-03-04 09:51:37 +0000

    Refactored to be slightly tidied and DRY

--- client/src/containers/GameContainer.js
+++ client/src/containers/GameContainer.js
@@ -12,89 +12,73 @@ const GameContainer = ({ playerOne, playerTwo, setPlayerOne, setPlayerTwo, quest
 
     const playerDirection = (event) => {
 
-    if (event.key === "d") {
-    playerOneInput.push("d")
-    console.log('p1 right', event.keyCode);
-    } else if (event.key === "s") {
-    playerOneInput.push("s")
-    console.log('p1 middle', event.keyCode)
-    } else if (event.key === "a") {
-    playerOneInput.push("a")
-    console.log('p1 left', event.keyCode)
-    } else if (event.key === "l") {
-    playerTwoInput.push("l")
-    console.log('p2 right', event.keyCode);
-    } else if (event.key === "k") {
-    playerTwoInput.push("k")
-    console.log('p2 middle', event.keyCode)
-    } else if (event.key === "j") {
-    playerTwoInput.push("j")
-    console.log('p2 left', event.keyCode)
-    }
-};
+        if (event.key === "d") {
+            playerOneInput.push("d")
+            console.log('p1 right', event.keyCode);
+        } else if (event.key === "s") {
+            playerOneInput.push("s")
+            console.log('p1 middle', event.keyCode)
+        } else if (event.key === "a") {
+            playerOneInput.push("a")
+            console.log('p1 left', event.keyCode)
+        } else if (event.key === "l") {
+            playerTwoInput.push("l")
+            console.log('p2 right', event.keyCode);
+        } else if (event.key === "k") {
+            playerTwoInput.push("k")
+            console.log('p2 middle', event.keyCode)
+        } else if (event.key === "j") {
+            playerTwoInput.push("j")
+            console.log('p2 left', event.keyCode)
+        }
+    };
 
 
     useEffect(() => {
-    window.addEventListener('keydown', playerDirection);
+        window.addEventListener('keydown', playerDirection);
 
-    return () => {
-    window.removeEventListener('keydown', playerDirection);
-    };
-}, []);
+        return () => {
+        window.removeEventListener('keydown', playerDirection);
+        };
+    }, []);
 
+    const checkGoal = () => {
 
-
-
-const checkGoal = () => {
-
-
-    const combinedInput = playerOneInput.slice(-1)[0].concat(playerTwoInput.slice(-1)[0])
-    console.log(combinedInput);
-
-    const scoreIndex = Math.floor(questionNumber / 2);
-    const playerGo = (questionNumber % 2 !== 0);
-
-    
-
-    if (combinedInput == ["dl"] || combinedInput == ["sk"] || combinedInput == ["aj"]) {
-        if (playerGo) {
-            let tempPlayer = playerOne
-            tempPlayer.score[scoreIndex] = 0
-            setPlayerOne(tempPlayer)
-        } else {
-            let tempPlayer = playerTwo
-            tempPlayer.score[scoreIndex - 1] = 0
-            setPlayerTwo(tempPlayer)
+        const combinedInput = playerOneInput.slice(-1)[0].concat(playerTwoInput.slice(-1)[0])
+        const scoreIndex = Math.floor(questionNumber / 2);
+        const playerGo = (questionNumber % 2 !== 0);
+        const adjustScore = (num) => {
+            if (playerGo) {
+                let tempPlayer = playerOne
+                tempPlayer.score[scoreIndex] = num
+                setPlayerOne(tempPlayer)
+            } else {
+                let tempPlayer = playerTwo
+                tempPlayer.score[scoreIndex - 1] = num
+                setPlayerTwo(tempPlayer)
+            }
         }
-    } else {
-        if (playerGo) {
-            let tempPlayer = playerOne
-            tempPlayer.score[scoreIndex] = 1
-            setPlayerOne(tempPlayer)
+        
+        if (combinedInput == ["dl"] || combinedInput == ["sk"] || combinedInput == ["aj"]) {
+            adjustScore(0);
+            console.log('It was saved!');
         } else {
-            let tempPlayer = playerTwo
-            tempPlayer.score[scoreIndex - 1] = 1
-            setPlayerTwo(tempPlayer)
+            adjustScore(1);
+            console.log('GOOOAL!');
         }
     }
-}
 
-        
     if (chosenAnswer === false){
         setTimeout(checkGoal, 1)
-        console.log(chosenAnswer)
         console.log("ANSWER INCORRECT")
     } else {
         setTimeout(checkGoal, 5000)
-        console.log(chosenAnswer)
         console.log("ANSWER CORRECT")
     }
 
     const handleClick = () => {
         setQuestionNumber(questionNumber + 1)
         setChosenAnswer(null)
-        console.log(chosenAnswer)
-
     }
 
     return (
