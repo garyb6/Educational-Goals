21c4e4437b661c89db1d5da5c8a8aaa26af93724
garyb6 <gary.bennett6@outlook.com>
2022-03-04 20:27:24 +0000

    kick sound works with animation starting onClick

--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -1,52 +1,54 @@
 import { typeImplementation } from '@testing-library/user-event/dist/type/typeImplementation';
-import React from 'react';
+import React, {useEffect, useSound} from 'react';
 
 
 const Game = ({ setPlayerOne, setPlayerTwo, questionNumber, playerOne, playerTwo}) => {
 
-  const playerOneInput = ["a"]
-  const playerTwoInput = ["l"]
-
-  const playerDirection = (event) => {
-
-    if (event.key === "d") {
-      playerOneInput.push("d")
-      console.log('p1 right', event.keyCode);
-    } else if (event.key === "s") {
-      playerOneInput.push("s")
-      console.log('p1 middle', event.keyCode)
-    } else if (event.key === "a") {
-      playerOneInput.push("a")
-      console.log('p1 left', event.keyCode)
-    } else if (event.key === "l") {
-      playerTwoInput.push("l")
-      console.log('p2 right', event.keyCode);
-    } else if (event.key === "k") {
-      playerTwoInput.push("k")
-      console.log('p2 middle', event.keyCode)
-    } else if (event.key === "j") {
-      playerTwoInput.push("j")
-      console.log('p2 left', event.keyCode)
-    }
-  };
 
 
-  React.useEffect(() => {
+    const playerOneInput = ["a"]
+    const playerTwoInput = ["l"]
+    const playerDirection = (event) => {
+
+        if (event.key === "d") {
+        playerOneInput.push("d")
+        console.log('p1 right', event.keyCode);
+        } else if (event.key === "s") {
+        playerOneInput.push("s")
+        console.log('p1 middle', event.keyCode)
+        } else if (event.key === "a") {
+        playerOneInput.push("a")
+        console.log('p1 left', event.keyCode)
+        } else if (event.key === "l") {
+        playerTwoInput.push("l")
+        console.log('p2 right', event.keyCode);
+        } else if (event.key === "k") {
+        playerTwoInput.push("k")
+        console.log('p2 middle', event.keyCode)
+        } else if (event.key === "j") {
+        playerTwoInput.push("j")
+        console.log('p2 left', event.keyCode)
+        }
+    };
+
+
+    useEffect(() => {
     window.addEventListener('keydown', playerDirection);
     setTimeout(checkGoal, 5000)
 
     return () => {
-      window.removeEventListener('keydown', playerDirection);
+        window.removeEventListener('keydown', playerDirection);
     };
-  }, []);
+    }, []);
 
-  const checkGoal = () => {
+    const checkGoal = () => {
 
     const combinedInput = playerOneInput.slice(-1)[0].concat(playerTwoInput.slice(-1)[0])
     console.log(combinedInput);
 
     if (combinedInput == ["dl"]) {
         return console.log("SAVE")
+        
     } else if (combinedInput == ["sk"]) {
         return console.log("SAVE")
     } else if (combinedInput == ["aj"]) {
@@ -63,8 +65,8 @@ const Game = ({ setPlayerOne, setPlayerTwo, questionNumber, playerOne, playerTwo
     }
         return console.log("GOOOOOOOOOAL")
     }
-  }
-  return (null);
+    }
+    return (null);
 };
 
 export default Game;
