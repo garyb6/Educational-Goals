72342e15f46d9347bb695b93f2e0318b7464b9b4
Jamie <jamienolan93@hotmail.co.uk>
2022-03-07 00:47:14 +0000

    added animations for different scenarios

--- client/src/components/Animation.js
+++ client/src/components/Animation.js
@@ -24,55 +24,123 @@ function Animation() {
 
 
             
-
-                <div className="keeper-container">
-                    <motion.img className='crouch'
-                    src={Crouch}
-                // style={{filter: "invert(16%) sepia(96%) saturate(6992%) hue-rotate(357deg) brightness(93%) contrast(118%)"}}
-
-
-                    animate={{
-                        // y: isAnimating ? 300 : 0,
-                        x: isAnimating ? 200 : 0,
-                        // opacity: isAnimating ? 1 : 0.5,
-                        // rotate: isAnimating ? 90:180,
-                    }}
-
-                    transition={{
-                        type: 'spring',
-                        duration: 0.8
-                    }}
-
-                    onClick={() => setIsAnimating(!isAnimating)}
-                    >
-
-                    </motion.img>
-                 </div>
-        
-                <div className="ball-container">
-                    <motion.img
-                        className="ball"
-                        src={Football}
-                        // style={{filter: "invert(18%) sepia(75%) saturate(5707%) hue-rotate(223deg) brightness(102%) contrast(105%)"}}
-                        // style={{filter: "invert(16%) sepia(96%) saturate(5992%) hue-rotate(357deg) brightness(93%) contrast(118%)"}}
+                    {/* dive right */}
+                    {/* <div className="keeper-container">
+                        <motion.img className='crouch'
+                        src={Crouch}
                         animate={{
-                            y: isAnimating ? 0 : 230,
-                            x: isAnimating ? 220 : 0,
-                            // opacity: isAnimating ? 1 : 0.5,
-                            rotate: isAnimating ? 360 : 0,
-                            scale: isAnimating ? 0.15 : 0.25,
-                        }}
-                        initial={{
-                            // opacity:0.1
+                            x: isAnimating ? 200 : 0,
+                            rotate: isAnimating ? 90:0,
                         }}
                         transition={{
-                            type: "spring",
-                            stiffness: 40
+                            type: 'spring',
+                            duration: 0.8
                         }}
+
                         onClick={() => setIsAnimating(!isAnimating)}
-                    >
-                    </motion.img>
-                </div>
+                        >
+
+                        </motion.img>
+                    </div> */}
+
+                    {/* dive left  */}
+                    {/* <div className="keeper-container">
+                        <motion.img className='crouch'
+                        src={Crouch}
+                        animate={{
+                            x: isAnimating ? -200 : 0,
+                            rotate: isAnimating ? -90:0,
+                        }}
+                        transition={{
+                            type: 'spring',
+                        duration: 0.8
+                        }}
+
+                        onClick={() => setIsAnimating(!isAnimating)}
+                        >
+
+                        </motion.img>
+                    </div>  */}
+
+                    {/* middle */}
+                    <div className="keeper-container">
+                        <motion.img className='crouch'
+                        src={Crouch}
+                        animate={{
+                            y: isAnimating ? 10 : 0,
+                        }}
+                        transition={{
+                            type: 'spring',
+                            duration: 0.8
+                        }}
+
+                        onClick={() => setIsAnimating(!isAnimating)}
+                        >
+
+                        </motion.img>
+                    </div>
+            
+                    {/* shoot right */}
+                    {/* <div className="ball-container">
+                        <motion.img
+                            className="ball"
+                            src={Football}
+                            animate={{
+                                y: isAnimating ? 0 : 230,
+                                x: isAnimating ? 220 : 0,
+                                rotate: isAnimating ? 360 : 0,
+                                scale: isAnimating ? 0.15 : 0.25,
+                            }}
+                            transition={{
+                                type: "spring",
+                                stiffness: 40
+                            }}
+                            onClick={() => setIsAnimating(!isAnimating)}
+                        >
+                        </motion.img>
+                    </div> */}
+
+                        {/* shoot left */}
+                        {/* <div className="ball-container">
+                        <motion.img
+                            className="ball"
+                            src={Football}
+                            animate={{
+                                y: isAnimating ? 0 : 230,
+                                x: isAnimating ? -220 : 0,
+                                rotate: isAnimating ? 360 : 0,
+                                scale: isAnimating ? 0.15 : 0.25,
+                            }}
+                            transition={{
+                                type: "spring",
+                                stiffness: 40
+                            }}
+                            onClick={() => setIsAnimating(!isAnimating)}
+                        >
+                        </motion.img>
+                    </div> */}
+
+                        {/* shoot middle score */}
+                        <div className="ball-container">
+                        <motion.img
+                            className="ball"
+                            src={Football}
+                            animate={{
+                                y: isAnimating ? 0 : 230,
+                                // x: isAnimating ? -220 : 0,
+                                rotate: isAnimating ? 360 : 0,
+                                scale: isAnimating ? 0.15 : 0.25,
+                            }}
+                            transition={{
+                                type: "spring",
+                                stiffness: 40
+                            }}
+                            onClick={() => setIsAnimating(!isAnimating)}
+                        >
+                        </motion.img>
+                    </div>
+
+                    
                 </div>
             {/* </div> */}
         </div>    
