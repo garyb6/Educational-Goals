c3647220c213d231dd375b66faf0ada4369edbf3
Jamie <jamienolan93@hotmail.co.uk>
2022-03-07 00:26:27 +0000

    added trophy animation to winning screen

--- client/src/containers/QuizContainer.js
+++ client/src/containers/QuizContainer.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { Link, Routes, Router, Route } from 'react-router-dom';
 import ScoreDisplay from '../components/ScoreDisplay';
+import Trophy from "../images/Trophy.png"
+import { motion } from 'framer-motion'
 import "./../css/QuizContainer.css"
 
 
@@ -15,9 +17,34 @@ const QuizContainer = ({ playerOne, playerTwo, selectedQuiz, requestQuiz, chosen
     if (questionNumber > 10) {
 
         if (playerOne.score.reduce((a, b) => a + b, 0) > playerTwo.score.reduce((a, b) => a + b, 0)){
-            return (<h1> {playerOne.name} is the winner!</h1>)
+            return (
+                <>
+                    <h1> {playerOne.name} is the winner!</h1>
+                    <div className='trophy-container'>
+                        <motion.img className='trophy'
+                            src={Trophy}
+                            drag
+                            dragConstraints={{ right:1,left:1,bottom:1,top: 1}}
+                            >
+                </motion.img>
+                <h4 className='winner-name'>{playerOne.name} Wins!</h4>
+                </div>
+                </>)
         } else if (playerTwo.score > playerOne.score) {
-            return (<h1> {playerTwo.name} is the winner!</h1>)
+            return (
+            <>
+                <h1> {playerTwo.name} is the winner!</h1>
+                <div className='trophy-container'>
+                    <motion.img className='trophy'
+                        src={Trophy}
+                        drag
+                        dragConstraints={{ right:1,left:1,bottom:1,top: 1}}
+                        >
+            </motion.img>
+            <h4 className='winner-name'>{playerTwo.name} Wins!</h4>
+            </div>
+            </>
+            )
         } else {
             return (<h1> It's a draw!</h1>)
         }
