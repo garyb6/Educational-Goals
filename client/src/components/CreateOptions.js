149406e7b8168a2a221de1c01bf1f377da18c6d3
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2022-03-08 11:28:55 +0000

    Adding required to all text boxes

--- client/src/components/CreateOptions.js
+++ client/src/components/CreateOptions.js
@@ -59,25 +59,25 @@ const CreateOptions = ({newQuestion, setNewQuestion, newQuiz, questionNumber, se
         <h6>Add options</h6>
         <form onSubmit={handleSubmit}>
         <label htmlFor="0option">Option 1:</label>
-        <input id="0option" type="text" onChange={handleChange}/>
+        <input id="0option" type="text" required onChange={handleChange}/>
         <select id="0select" onChange={handleChange}>
             <option value={false}>Not correct</option>
             <option value={true}>Correct</option>
         </select>
         <label htmlFor="1option">Option 2:</label>
-        <input id="1option" type="text" onChange={handleChange}/>
+        <input id="1option" type="text" required onChange={handleChange}/>
         <select id="1select" onChange={handleChange}>
             <option value={false}>Not correct</option>
             <option value={true}>Correct</option>
         </select>
         <label htmlFor="2option">Option 3:</label>
-        <input id="2option" type="text" onChange={handleChange}/>
+        <input id="2option" type="text" required onChange={handleChange}/>
         <select id="2select" onChange={handleChange}>
             <option value={false}>Not correct</option>  
             <option value={true}>Correct</option>
         </select>
         <label htmlFor="3option">Option 4:</label>
-        <input id="3option" type="text" onChange={handleChange}/>
+        <input id="3option" type="text" required onChange={handleChange}/>
         <select id="3select" onChange={handleChange}>
             <option value={false}>Not correct</option>
             <option value={true}>Correct</option>
