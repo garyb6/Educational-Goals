aa21a50b87ec5c94b4f6816db71f5818afda21a8
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2022-03-08 11:55:44 +0000

    More colours in styles

--- client/src/data/Styles.js
+++ client/src/data/Styles.js
@@ -1,8 +1,18 @@
 export const styles = [
     // Red
-    { filter: "invert(24%) sepia(45%) saturate(6956%) hue-rotate(352deg) brightness(105%) contrast(85%)" }, 
+    { filter: "invert(10%) sepia(82%) saturate(6458%) hue-rotate(0deg) brightness(93%) contrast(112%)" }, 
     // Green
-    { filter: "invert(67%) sepia(13%) saturate(4833%) hue-rotate(72deg) brightness(101%) contrast(81%)" },
+    { filter: "invert(41%) sepia(68%) saturate(1330%) hue-rotate(87deg) brightness(101%) contrast(111%)" },
     // Blue
-    { filter: "invert(18%) sepia(75%) saturate(5707%) hue-rotate(223deg) brightness(102%) contrast(105%)" }
+    { filter: "invert(25%) sepia(98%) saturate(1703%) hue-rotate(197deg) brightness(90%) contrast(104%)" },
+    // Yellow
+    { filter: "invert(83%) sepia(52%) saturate(1545%) hue-rotate(2deg) brightness(108%) contrast(103%)" },
+    // Pink
+    { filter: "invert(27%) sepia(74%) saturate(7263%) hue-rotate(294deg) brightness(115%) contrast(126%)" },
+    // Orange
+    { filter: "invert(79%) sepia(59%) saturate(4281%) hue-rotate(359deg) brightness(106%) contrast(105%)" },
+    // Purple
+    { filter: "invert(13%) sepia(100%) saturate(4928%) hue-rotate(278deg) brightness(80%) contrast(116%)" },
+    // Black
+    { filter: "" }
 ]
