ecbcd2069146bb030923ad11bb8355f55db3e266
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2022-03-08 12:02:40 +0000

    Changing styles to an object

--- client/src/data/Styles.js
+++ client/src/data/Styles.js
@@ -1,18 +1,10 @@
-export const styles = [
-    // Red
-    { filter: "invert(10%) sepia(82%) saturate(6458%) hue-rotate(0deg) brightness(93%) contrast(112%)" }, 
-    // Green
-    { filter: "invert(41%) sepia(68%) saturate(1330%) hue-rotate(87deg) brightness(101%) contrast(111%)" },
-    // Blue
-    { filter: "invert(25%) sepia(98%) saturate(1703%) hue-rotate(197deg) brightness(90%) contrast(104%)" },
-    // Yellow
-    { filter: "invert(83%) sepia(52%) saturate(1545%) hue-rotate(2deg) brightness(108%) contrast(103%)" },
-    // Pink
-    { filter: "invert(27%) sepia(74%) saturate(7263%) hue-rotate(294deg) brightness(115%) contrast(126%)" },
-    // Orange
-    { filter: "invert(79%) sepia(59%) saturate(4281%) hue-rotate(359deg) brightness(106%) contrast(105%)" },
-    // Purple
-    { filter: "invert(13%) sepia(100%) saturate(4928%) hue-rotate(278deg) brightness(80%) contrast(116%)" },
-    // Black
-    { filter: "" }
-]
+export const styles = {
+    red: { filter: "invert(10%) sepia(82%) saturate(6458%) hue-rotate(0deg) brightness(93%) contrast(112%)" }, 
+    green: { filter: "invert(41%) sepia(68%) saturate(1330%) hue-rotate(87deg) brightness(101%) contrast(111%)" },
+    blue: { filter: "invert(25%) sepia(98%) saturate(1703%) hue-rotate(197deg) brightness(90%) contrast(104%)" },
+    yellow: { filter: "invert(83%) sepia(52%) saturate(1545%) hue-rotate(2deg) brightness(108%) contrast(103%)" },
+    pink: { filter: "invert(27%) sepia(74%) saturate(7263%) hue-rotate(294deg) brightness(115%) contrast(126%)" },
+    orange: { filter: "invert(79%) sepia(59%) saturate(4281%) hue-rotate(359deg) brightness(106%) contrast(105%)" },
+    purple: { filter: "invert(13%) sepia(100%) saturate(4928%) hue-rotate(278deg) brightness(80%) contrast(116%)" },
+    black: { filter: "" }
+}
