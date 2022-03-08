60af4e2af35054d861ef091c60bd7a3a4a9c7809
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2022-03-08 12:33:24 +0000

    Tweaking colours slightly

--- client/src/data/Styles.js
+++ client/src/data/Styles.js
@@ -1,11 +1,11 @@
 export const styles = [
-    {colour: "Red", filter: { filter: "invert(10%) sepia(82%) saturate(6458%) hue-rotate(0deg) brightness(93%) contrast(112%)" }}, 
+    {colour: "Red", filter: { filter: "invert(20%) sepia(48%) saturate(7450%) hue-rotate(355deg) brightness(74%) contrast(123%)" }}, 
     {colour: "Green", filter: { filter: "invert(41%) sepia(68%) saturate(1330%) hue-rotate(87deg) brightness(101%) contrast(111%)" }},
     {colour: "Blue", filter: { filter: "invert(25%) sepia(98%) saturate(1703%) hue-rotate(197deg) brightness(90%) contrast(104%)" }},
     {colour: "Yellow", filter: { filter: "invert(83%) sepia(52%) saturate(1545%) hue-rotate(2deg) brightness(108%) contrast(103%)" }},
     {colour: "Pink", filter: { filter: "invert(27%) sepia(74%) saturate(7263%) hue-rotate(294deg) brightness(115%) contrast(126%)" }},
-    {colour: "Orange", filter: { filter: "invert(79%) sepia(59%) saturate(4281%) hue-rotate(359deg) brightness(106%) contrast(105%)" }},
+    {colour: "Orange", filter: { filter: "invert(68%) sepia(48%) saturate(4075%) hue-rotate(1deg) brightness(107%) contrast(104%)" }},
     {colour: "Purple", filter: { filter: "invert(13%) sepia(100%) saturate(4928%) hue-rotate(278deg) brightness(80%) contrast(116%)" }},
     {colour: "White", filter: { filter: "invert(99%) sepia(6%) saturate(282%) hue-rotate(301deg) brightness(117%) contrast(94%)" }},
-    {colour: "Black", filter: { filter: "" }}
+    {colour: "Black", filter: { filter: "invert(23%) sepia(1%) saturate(0%) hue-rotate(159deg) brightness(102%) contrast(87%)" }}
 ]
