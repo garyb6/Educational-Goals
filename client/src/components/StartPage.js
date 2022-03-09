38cdbbc2a4d8cf8b2f8dffee1a766753e086f50f
Jamie <jamienolan93@hotmail.co.uk>
2022-03-09 11:02:20 +0000

    how to play created

--- client/src/components/StartPage.js
+++ client/src/components/StartPage.js
@@ -12,6 +12,7 @@ const StartPage = () => {
             </div>
             <Link to="/start" className="startPageButton"><div className="startPageButtonText">Play a quiz</div></Link>
             <Link to="/quizzes/create" className="startPageButton"><div className="startPageButtonText">Create a quiz</div></Link>
+            <Link to="/start/instructions" className="startPageButton"><div className="startPageButtonText">How to play</div></Link>
         </div>
         </>
     )
